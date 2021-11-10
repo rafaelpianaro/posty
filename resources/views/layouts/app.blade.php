ee6d9806c261ea694963d6cc96632df6eb198d46
Rafael <rafaelpianaro@gmail.com>
2021-11-10 18:26:44 -0300

    install tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Posty</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body>
+<body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">Dashboard</a>
+            </li>
+            <li>
+                <a href="">Post</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
