95a1bb8e4b8e6d22220b01a1b5bf7b4ae6459931
Rafael <rafaelpianaro@gmail.com>
2021-11-11 18:48:29 -0300

    add views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,17 +7,31 @@
     <title>Posty</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body class="bg-gray-100">
-    <nav class="p-6 bg-white flex justify-between">
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
             <li>
-                <a href="">Home</a>
+                <a href="" class="p-3">Home</a>
             </li>
             <li>
-                <a href="">Dashboard</a>
+                <a href="" class="p-3">Dashboard</a>
             </li>
             <li>
-                <a href="">Post</a>
+                <a href="" class="p-3">Post</a>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">João Gottardi</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Register</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Logout</a>
             </li>
         </ul>
     </nav>
