cfe530742596067d4335846cb224aff48040c8f7
Rafael <rafaelpianaro@gmail.com>
2021-11-12 19:59:17 -0300

    added like and unlike

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -25,6 +25,16 @@
                         <a href="" class="font-bold">{{ $post->user->username }}</a>
                         <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb-2">{{ $post->body }}</p>
+                        <div class="flex items-center">
+                            <form action="" method="post" class="mr-1">
+                                @csrf
+                                <button type="submit" class="text-blue-500">Like</button>
+                            </form>
+                            <form action="" method="post" class="mr-1">
+                                @csrf
+                                <button type="submit" class="text-gray-500">Unlike</button>
+                            </form>
+                        </div>
                     </div>
                 @endforeach
                 {{ $posts->links() }}
