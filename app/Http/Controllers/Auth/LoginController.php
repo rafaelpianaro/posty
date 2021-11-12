4af663ea012dff567cba2dfb93de666a4f81ec1f
Rafael <rafaelpianaro@gmail.com>
2021-11-12 14:11:24 -0300

    set remember me

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,11 +19,12 @@ class LoginController extends Controller
 
     public function store(Request $request)
     {
+        // dd($request->remember);
         $this->validate($request,[
             'email' => 'required|email',
             'password' => 'required',
         ]);
-        if(!auth()->attempt($request->only('email','password'))){
+        if(!auth()->attempt($request->only('email','password'),$request->remember)){
             return back()->with('status', 'Invalid login details');
         }
         return redirect()->route('dashboard');
