1cbd6372d6f1b41bac2c2782f527a2ea68e7e112
Rafael <rafaelpianaro@gmail.com>
2021-11-11 22:20:02 -0300

    save user on db

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,11 +3,36 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function index() {
         return view('auth.register');
+    }
+
+    public function store(Request $request) {
+        // dd($request->get('email'));
+        // dd($request->email);
+        $this->validate($request,[
+            'nome' => 'required|max:255|min:2',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+        ],
+        [
+            'nome.required' => 'Campo obrigatório.',
+            'nome.min' => ':attribute precisa conter ao menos :min caracteres.'
+        ]
+    );
+        // dd('store');
+        User::create([
+            'name' => $request->nome,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
     }
 }
