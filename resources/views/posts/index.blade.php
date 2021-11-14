cad1f92b609938b4594f98f442cede91eed87547
Rafael <rafaelpianaro@gmail.com>
2021-11-14 20:21:35 -0300

    post of user

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -22,7 +22,7 @@
             @if ($posts->count())
                 @foreach ($posts as $post)
                     <div class="mb-4">
-                        <a href="" class="font-bold">{{ $post->user->username }}</a>
+                        <a href="{{ route('users.posts', $post->user) }}" class="font-bold">{{ $post->user->username }}</a>
                         <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb-2">{{ $post->body }}</p>
                         {{-- use policy (more clean) --}}
