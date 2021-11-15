9bac70b0d1cbaa091e7dcc9548caca2517384959
Rafael <rafaelpianaro@gmail.com>
2021-11-14 21:06:19 -0300

    *components of post *individual post page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,6 +15,11 @@ class PostController extends Controller
         return view('posts.index',['posts' => $posts]);
     }
 
+    public function show(Post $post)
+    {
+        return view('posts.show', ['post' => $post]);
+    }
+
     public function store(Request $request)
     {
         $this->validate($request, [
